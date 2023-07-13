15582be2522d4b3392e873c0edda7b49cbf557ea
travistran <travistran544@gmail.com>
2023-07-13 11:17:00 -0400

    worked on bank test

--- test_acc.py
+++ test_acc.py
@@ -6,6 +6,8 @@ class TestAcc(unittest.TestCase):
     def test_acc(self):
         travis = BankAcc.BankAcc("Travis", 100, 0)
         self.assertEqual(travis.name, "Travis")
+        self.assertEqual(travis.accNumber, 100)
+        self.assertEqual(travis.balance, 0)
 
 
 if __name__ == "__main__":
